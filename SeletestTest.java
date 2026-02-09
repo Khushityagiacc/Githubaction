2bf1c08eb78af9504fee96f83d7d57eed583e207
Khushityagiacc <khushi.a.tyagi@accenture.com>
2026-02-09 16:59:30 +0530

    Update SeletestTest.java

--- SeletestTest.java
+++ SeletestTest.java
@@ -1,107 +1,115 @@
-
- 
 import java.time.Duration;
 import java.util.List;
- 
+
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.StaleElementReferenceException;
-import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 import org.testng.annotations.*;
- 
+
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
- 
-import Fedex.PDRENTAL.ConfigReader;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import Fedex.Testngtestcase.Extenrreport;
 import Fedex.Testngtestcase.Screenshot;
-import Fedex.convertedcodes.SPAdvancesearch.TestUtils;
- 
- 
+
 public class SeletestTest {
-	 static WebDriver driver;
-	    static WebDriverWait wait;
-	    ExtentReports extent;
-	    static ExtentTest test;
- 
-	    @BeforeTest
-	    public void launchBrowser() {
-	        extent = Extenrreport.getInstance(); // Initialize Extent Reports
-	        test = extent.createTest("Maximo Login Test");
- 
-	         WebDriverManager.chromedriver().setup();
+
+    static WebDriver driver;
+    static WebDriverWait wait;
+    static ExtentReports extent;
+    static ExtentTest test;
+
+    // =========================
+    // SETUP
+    // =========================
+    @BeforeTest
+    public void launchBrowser() {
+
+        extent = Extenrreport.getInstance();
+        test = extent.createTest("Accenture Careers Search Test");
+
+        WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");      // required for GitHub Actions
+        options.addArguments("--headless=new"); // GitHub Actions
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
 
-       driver.get("https://www.accenture.com/in-en/careers");
-        System.out.println(driver.getTitle());
+        driver = new ChromeDriver(options);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        driver.quit();
-	    }
-	    @Test
-	    public void test() throws InterruptedException {
-	    		    	List<WebElement> frames = driver.findElements(By.tagName("iframe"));
-	    	System.out.println("Total iframes on the page: " + frames.size());
-	    	for (int i = 0; i < frames.size(); i++) {
-	    	    System.out.println("Frame " + i + ": id=" + frames.get(i).getAttribute("id") + ", name=" + frames.get(i).getAttribute("name"));
-	    	}
- 
-	    	boolean elementFound = false;
-	    	for (int i = 0; i < frames.size(); i++) {
-	    	    try {
-	    	    	driver.switchTo().defaultContent();
-	    	        driver.switchTo().frame(frames.get(i));
-	    	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
-	    	      
-	    	   
-	    	      
-	    	        // ✅ Click Search Button
-	    	      WebElement searchButton = wait.until(
-    ExpectedConditions.elementToBeClickable(
-        By.cssSelector(".rad-header__search-button")
-    )
-);
+        driver.manage().window().maximize();
+        driver.get("https://www.accenture.com/in-en/careers");
 
-Screenshot.highlightElement(driver, searchButton);
-String searchScreenshot = Screenshot.takeScreenshot(driver, "search_button");
-searchButton.click();
+        test.pass("Browser launched and navigated to Accenture Careers page");
+    }
 
-test.pass("Click on Need to Know",
-    MediaEntityBuilder.createScreenCaptureFromPath(searchScreenshot).build());
+    // =========================
+    // TEST
+    // =========================
+    @Test
+    public void clickSearchButtonInsideIframe() {
 
-	    	      
-	    	        
-	    	        
-	    	        
-	    	        
-	    	        elementFound = true;
-	    	        
-	    	        
-	    	        
-	    	        // Found and done - exit the iframe loop
-	    	        
-	    	        
-	    	   break;
-	    	   	            } catch (Exception e) {
-	    	   	                System.out.println("Element not found in frame index: " + i + " - " + e.getMessage());
-	    	   	                // Switch back to default content before next iteration
-	    	   	                driver.switchTo().defaultContent();
-	    	   	            }
-	    	   	        }
-	    }
- 
+        List<WebElement> frames = driver.findElements(By.tagName("iframe"));
+        test.info("Total iframes found: " + frames.size());
+
+        boolean elementFound = false;
+
+        for (int i = 0; i < frames.size(); i++) {
+            try {
+                driver.switchTo().defaultContent();
+                driver.switchTo().frame(frames.get(i));
+
+                WebElement searchButton = wait.until(
+                        ExpectedConditions.elementToBeClickable(
+                                By.cssSelector(".rad-header__search-button")
+                        )
+                );
+
+                Screenshot.highlightElement(driver, searchButton);
+                String screenshotPath =
+                        Screenshot.takeScreenshot(driver, "search_button_iframe_" + i);
+
+                searchButton.click();
+
+                test.pass(
+                        "Clicked Search button in iframe index: " + i,
+                        MediaEntityBuilder
+                                .createScreenCaptureFromPath(screenshotPath)
+                                .build()
+                );
+
+                elementFound = true;
+                break;
+
+            } catch (Exception e) {
+                test.info("Search button not found in iframe index: " + i);
+                driver.switchTo().defaultContent();
+            }
+        }
+
+        if (!elementFound) {
+            test.fail("Search button not found in any iframe");
+        }
+    }
+
+    // =========================
+    // TEARDOWN
+    // =========================
+    @AfterTest
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+
+        extent.flush();
+    }
 }
- 
-
- 
