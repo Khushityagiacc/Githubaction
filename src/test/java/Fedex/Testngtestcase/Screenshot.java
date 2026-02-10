505cb3164ebd37f8ae441903a67ad3851ebb2bc4
Khushityagiacc <khushi.a.tyagi@accenture.com>
2026-02-10 14:59:46 +0530

    Update Screenshot.java

--- src/test/java/Fedex/Testngtestcase/Screenshot.java
+++ src/test/java/Fedex/Testngtestcase/Screenshot.java
@@ -1,33 +1,19 @@
 package Fedex.Testngtestcase;
 
 import java.io.File;
-
 import java.io.IOException;
-
 import java.nio.file.Files;
-
 import java.text.SimpleDateFormat;
-
 import java.util.Date;
 
+import org.openqa.selenium.*;
 import org.openqa.selenium.JavascriptExecutor;
-
-import org.openqa.selenium.OutputType;
-
-import org.openqa.selenium.TakesScreenshot;
-
-import org.openqa.selenium.WebDriver;
-
-import org.openqa.selenium.WebElement;
 
 public class Screenshot {
 
     public static void highlightElement(WebDriver driver, WebElement element) {
-
         JavascriptExecutor js = (JavascriptExecutor) driver;
-
         js.executeScript("arguments[0].style.border='3px solid red'", element);
-
     }
 
     public static String takeScreenshot(WebDriver driver, String name) {
@@ -35,26 +21,17 @@ public class Screenshot {
         File src = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 
         String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-
-        String relativePath = "screenshots/" + name + "_" + timestamp + ".png";
-
-        File dest = new File("test-output/" + relativePath);
+        String screenshotDir = "test-output/screenshots";
+        String screenshotPath = screenshotDir + "/" + name + "_" + timestamp + ".png";
 
         try {
-
-            Files.createDirectories(dest.getParentFile().toPath());
-
-            Files.copy(src.toPath(), dest.toPath());
-
+            Files.createDirectories(new File(screenshotDir).toPath());
+            Files.copy(src.toPath(), new File(screenshotPath).toPath());
         } catch (IOException e) {
-
             System.out.println("Screenshot failed: " + e.getMessage());
-
         }
 
-        return relativePath;
-
+        // ✅ EXTENT NEEDS PATH RELATIVE TO REPORT LOCATION
+        return "screenshots/" + name + "_" + timestamp + ".png";
     }
-
 }
- 
